19b78fb2e6daf3496d402b8d814af2d8e6793ce0
Sebastian Luzynski <sebastian.luzynski@gmail.com>
2023-03-09 21:10:34 +0100

    Revert "Prepare Vectors for generic size parameter"
    This reverts commit d24042af49800d1dac52b47f828f0895837d35ea.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-// mod basic_types;
-// pub use basic_types::{Point, Point3, Point3f32, Point3f64, Tuple, Vec3, Vec3f32, Vec3f64, Vector};
+mod basic_types;
+pub use basic_types::{Point, Point3, Point3f32, Point3f64, Tuple, Vec3, Vec3f32, Vec3f64, Vector};
 
-// mod matrix;
-mod vector;
+mod matrix;
